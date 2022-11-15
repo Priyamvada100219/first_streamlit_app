128c9495811b1dd646b9bafa64ad38bc3c8f79cd
Priyamvada <118182997+Priyamvada100219@users.noreply.github.com>
2022-11-15 11:14:07 +0530

    Update streamlit_app.py
    Changed break fast Menu and added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 streamlit.title('Hello ,My parents new healthy Dinner.')
 
 streamlit.header('Breakfast Menu')
-streamlit. text('Idly')
-streamlit.text('Sambar Idly')
-streamlit.text('Kata Idly')
-streamlit.text('Guntur Idly')
+streamlit. text('🥣 Omega 3 & Bluberry oatmeal')
+streamlit.text('🥗 Kale,Spinach & Rocket Smoothie')
+streamlit.text('🍞 Hard-Boiled Free-Range Egg Sandwich ')
+streamlit.text('🥑 Avacado Toast')
