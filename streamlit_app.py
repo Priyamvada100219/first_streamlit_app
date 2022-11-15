b7b4605b1da76ff9d83338c7b8bf95657f5b512c
Priyamvada <118182997+Priyamvada100219@users.noreply.github.com>
2022-11-15 11:03:21 +0530

    Update streamlit_app.py
    Added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('Hello ,My parents new healthy Dinner.')
+
+streamlit.header('Breakfast Menu')
+streamlit. text('Idly')
+streamlit.text('Sambar Idly')
+streamlit.text('Kata Idly')
+streamlit.text('Guntur Idly')
